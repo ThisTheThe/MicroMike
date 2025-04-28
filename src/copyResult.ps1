851a8f8cff3823dd845c5cbe442b0f65b87fd796
GitUser <git@git.info>
2025-04-28 13:43:19 -0600

    Fix reported issues.

--- src/copyResult.ps1
+++ src/copyResult.ps1
@@ -1,7 +1,7 @@
 $sourcePath = "C:\pim3\MicroMikeGit\MicroMike\src\theme.css"
 $destPath = "C:\pim3\newtekgit\.obsidian\themes\Art Deco CSS\theme.css"
 $destPath2 = "C:\pim3\MicroMikeGit\MicroMike\theme.css"
-$destPath3 = "C:\pim3\MMpure\.obsidian\themes\micro mike 2\theme.css"
+$destPath3 = "C:\pim3\MMpure\.obsidian\themes\Art Deco CSS\theme.css"
 # Copy to first destination
 Copy-Item -Path $sourcePath -Destination $destPath -Force
 Write-Host "File copied to $destPath"
